24691e40ea3b96bdb43adc6dbc03a94666c2a3d0
johncle712 <johncle712@gmail.com>
2022-06-29 20:10:59 +0000

    Created new script that adds a user

---
+++ add_user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo
+
+echo "Please enter the username to create: "
+
+echo
+
+read username
+
+echo
+
+cut -d: -f1 /etc/passwd | grep -q "\b$username\b"
+
+if [ $? -eq 0 ]; then
+	echo "The User ID \"$username\" already exists!!"
+	echo
+else
+	sudo useradd -m $username
+	echo "The User ID \"$username\" has been created successfully!"
+	echo
+fi
