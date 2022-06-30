4fc0d0d0e131623238c0591d29778af82a5a3c0c
John Le <johncle712@gmail.com>
2022-06-30 05:25:28 +0000

    Modified script to add new logic to pass arguments to create multiple users

--- add_user.sh
+++ add_user.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 echo
 
-echo "Please enter the username to create: "
-
-echo
-
-read username
-
-echo
-
-cut -d: -f1 /etc/passwd | grep -q "\b$username\b"
-
-if [ $? -eq 0 ]; then
-	echo "The User ID \"$username\" already exists!!"
-	echo
-else
-	sudo useradd -m $username
-	echo "The User ID \"$username\" has been created successfully!"
-	echo
-fi
+for username in "$@"; do
+	cut -d: -f1 /etc/passwd | grep -q "\b$username\b"
+	if [ $? -eq 0 ]; then
+		echo "The User ID \"$username\" already exists!!"
+		echo
+	else
+		sudo useradd -m $username
+		echo "The User ID \"$username\" has been created successfully!"
+		echo
+	fi
+done
